528ebf8cf9a359547587a288cb31283cae2e43cf
jaydip1235 <jaydipdeyalok@gmail.com>
2021-04-15 15:34:05 +0530

    Second commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
-import '../App.css';
+
+import {data} from '../data';
+import Navbar from './Navbar';
+import MovieCard from './MovieCard';
 
 function App() {
   return (
     <div className="App">
-        
+        <Navbar/>
+        <div className="main">
+          <div className="tabs">
+            <div className="tab">Movies</div>
+            <div className="tab">Favorites</div>
+        </div>
+
+        <div className="list">
+            {
+              data.map(movie => (
+                <MovieCard movie={movie} />
+              ))
+            }
+        </div>
+
+        </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,5 @@ import './index.css';
 import App from './components/App';
 
 
-ReactDOM.render(<React.StrictMode> <App /></React.StrictMode>,
-  document.getElementById('root'));
+ReactDOM.render(<React.StrictMode> <App />  </React.StrictMode>,document.getElementById('root'));
 
