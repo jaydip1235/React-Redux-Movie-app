88f2c08c9227d364f125c6f702457eab876c4251
jaydip1235 <jaydipdeyalok@gmail.com>
2021-04-17 08:43:54 +0530

    Done with search_result

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,6 +2,8 @@ export const ADD_MOVIES = 'ADD_MOVIES';
 export const ADD_TO_FAVOURITES = 'ADD_TO_FAVOURITES';
 export const REMOVE_FROM_FAVOURITES = 'REMOVE_FROM_FAVOURITES';
 export const SET_SHOW_FAVOURITES = 'SET_SHOW_FAVOURITES';
+export const ADD_SEARCH_RESULT = 'ADD_SEARCH_RESULT';
+export const ADD_MOVIE_TO_LIST = "ADD_MOVIE_TO_LIST";
 
 //action creators
 export function addMovies(movies){
@@ -30,3 +32,29 @@ export function setShowFavourties(val){
         val
     }
 }
+
+export function handleMovieSearch(movie){
+    const url = `http://www.omdbapi.com/?t=${movie}&apikey=675c4427`
+
+    return function (dispatch){
+        fetch(url)
+        .then(response=>response.json())
+        .then(movie=>{
+            
+            //dispatch an action
+            dispatch(addMovieSearchResult(movie))
+        })
+    }
+}
+export function addMovieSearchResult(movie){
+    return {
+        type: ADD_SEARCH_RESULT ,
+         movie
+        };
+}
+export function addMovieToList(movie) {
+    return {
+      type: ADD_MOVIE_TO_LIST,
+      movie,
+    };
+  }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,56 @@
-import React from 'react'
-
+import React from "react";
+import { handleMovieSearch, addMovieToList } from "../actions";
 
 class Navbar extends React.Component {
-    render() {
-        return (
-                <div className="nav">
-                    <div className="search-container">
-                        <input/>
-                        <button id="search-btn">Search</button>
-                    </div>
+  state = {
+    showSearchResults: false,
+    searchText: "",
+  };
+  
+  handleAddToMovies = (movie) => {
+    this.props.dispatch(addMovieToList(movie));
+    this.setState({
+      showSearchResults: false,
+    });
+  };
+
+  handleSearch = () => {
+    const { searchText } = this.state;
+    console.log("search");
+    this.props.dispatch(handleMovieSearch(searchText));
+  };
+  handleChange = (e) => {
+    this.setState({
+      searchText: e.target.value,
+    });
+  };
+  render() {
+    const { result, showSearchResults } = this.props.search;
+    return (
+      <div className="nav">
+        <div className="search-container">
+          <input onChange={this.handleChange} />
+          <button id="search-btn" onClick={this.handleSearch}>
+            Search
+          </button>
+
+          {showSearchResults && (
+            <div className="search-results">
+              <div className="search-result">
+                <img src={result.Poster} alt="search-pic" />
+                <div className="movie-info">
+                  <span>{result.title}</span>
+                  <button onClick={() => this.handleAddToMovies(result)}>
+                    Add to movies
+                  </button>
                 </div>
-        );
-    }
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
-export default Navbar ;
+
+export default Navbar;
